cb405fd8d93b202a641ba2eecec69c4a8e41737d
Kawashima.Mitsuhiro <kawashima.mitsuhiro@vareal.co.jp>
2023-02-12 15:16:37 +0900

    form_withのオプションを修正

--- teamE/app/controllers/gourmet_posts_controller.rb
+++ teamE/app/controllers/gourmet_posts_controller.rb
@@ -23,6 +23,6 @@ class GourmetPostsController < ApplicationController
   private
 
   def gourmet_post_params
-    params.permit(:title, :content, :picture)
+    params.require(:post).permit(:title, :content, :picture)
   end
 end
