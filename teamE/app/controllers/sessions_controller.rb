8a0b032f745eb49c0aab0030d332064bc91738ea
MatsubuchiTomoki <matsubuchi.tomoki@vareal.co.jp>
2023-02-15 17:12:29 +0900

    フィールド赤枠対応とエラーメッセージ下に表示対応

--- teamE/app/controllers/sessions_controller.rb
+++ teamE/app/controllers/sessions_controller.rb
@@ -5,13 +5,24 @@ before_action :login_required, only: :destroy
   end
 
   def create
-    user = User.find_by(login_id: params[:session][:login_id])
-    if user &.authenticate(params[:session][:password])
+    login_id = params[:session][:login_id]
+    password = params[:session][:password]
+    user = User.find_by(login_id: login_id)
+    if user &.authenticate(password)
       login(user)
       flash[:success] = "ログインしました。"
       redirect_to gourmet_posts_path
     else
-      flash[:danger] = 'IDかパスワードが間違っています。'
+      if login_id.blank?
+        @login_error = 'ログインIDが空欄です。'
+      else
+        @login_error = 'ログインIDが間違っています。'
+      end
+      if password.blank?
+        @password_error = 'パスワードが空欄です。'
+      else
+        @password_error = 'パスワードが間違っています。'
+      end
       render :new
     end
   end
