c0c4696fa67e6425390c573720b79862d636a32d
MatsubuchiTomoki <matsubuchi.tomoki@vareal.co.jp>
2023-02-21 18:48:25 +0900

    #41対応

--- teamE/app/models/user.rb
+++ teamE/app/models/user.rb
@@ -14,5 +14,14 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :login_id, presence: true, uniqueness: true, length: {maximum: 30}
   has_secure_password(validations: false)
-  validates :password, format: { with: /[a-zA-Z0-9]/ }, on: :create
+
+  validate do |record|
+    record.errors.add(:password, :blank) unless record.password_digest.present?
+  end
+
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "は半角英数字のみが使用できます" }
+  validates_length_of :password, maximum: ActiveModel::SecurePassword::MAX_PASSWORD_LENGTH_ALLOWED
+  validates_confirmation_of :password, allow_blank: true
+
+  validates :password_confirmation, presence: true
 end
