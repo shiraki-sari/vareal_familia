709f502e2e6245d035757907d2214a9039445193
matsubuchi-tomoki <124134434+matsubuchi-tomoki@users.noreply.github.com>
2023-03-01 17:21:19 +0900

    Revert "[TeamE] ログインユーザーと投稿を紐付けたい #39"

--- teamE/db/schema.rb
+++ teamE/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_01_072441) do
+ActiveRecord::Schema.define(version: 2023_02_08_072229) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -45,8 +45,6 @@ ActiveRecord::Schema.define(version: 2023_03_01_072441) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -59,5 +57,4 @@ ActiveRecord::Schema.define(version: 2023_03_01_072441) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "posts", "users"
 end
