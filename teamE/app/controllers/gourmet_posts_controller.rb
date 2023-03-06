37cc0caa93bfbb67f9095b277b0e494ff494c8b3
mukaide-tomoo <126652792+mukaide-tomoo@users.noreply.github.com>
2023-03-06 13:36:51 +0900

    投稿削除処理の修正
    ・if文の簡略化
    ・投稿者とログインユーザーが一致する投稿を取得するメソッド追加+before_action追加
    ・削除後のメッセージをflashで表示させる（ログイン時flashのようにスタイルを統一する）

--- teamE/app/controllers/gourmet_posts_controller.rb
+++ teamE/app/controllers/gourmet_posts_controller.rb
@@ -1,6 +1,7 @@
 class GourmetPostsController < ApplicationController
   before_action :login_required
   skip_before_action :login_required, only: :index
+  before_action :set_user_post, only: [:destroy]
 
   def index
     @posts = Post.with_attached_picture.order(updated_at: "DESC")
@@ -25,18 +26,18 @@ class GourmetPostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
-    if @post.user == @current_user
-      @post.destroy # MEMO: 投稿者がいない投稿はコンソールから削除する仕様
-      redirect_to gourmet_posts_path, notice: '投稿を削除しました。'
-    else
-      redirect_to gourmet_post_path(params[:id]), alert: '投稿したユーザーのみ削除可能です。'
-    end
+    return redirect_to gourmet_post_path(params[:id]), danger: '投稿したユーザーのみ削除可能です。' unless @post
+    @post.destroy # MEMO: 投稿者がいない投稿はコンソールから削除する仕様
+    redirect_to gourmet_posts_path, success: '投稿を削除しました。'
   end
 
   private
 
   def gourmet_post_params
     params.require(:post).permit(:title, :content, :picture)
+  end
+
+  def set_user_post
+    @post = Post.find_by(id: params[:id], user_id: current_user.id)
   end
 end
